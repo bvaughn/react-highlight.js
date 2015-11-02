200516ebbbc557cfb2de5ac98b7cc283f649f0fa
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-11-02 15:19:16 -0800

    Added a test

--- tests/Highlight.test.js
+++ tests/Highlight.test.js
@@ -1,9 +1,26 @@
-import React, { Component, PropTypes } from 'react'
+import React from 'react'
 import { findDOMNode } from 'react-dom'
-import TestUtils from 'react-addons-test-utils'
 import expect from 'expect.js'
 import Highlight from '../source/Highlight.jsx'
 
 describe('Highlight', () => {
-  // TODO
+  const language = 'javascript'
+  const content = 'var foo = "Foo"; // comment'
+  const node = document.createElement('div')
+  const highlight = React.render(
+    <Highlight language={language}>
+      {content}
+    </Highlight>,
+    node
+  )
+  const highlightDOMNode = findDOMNode(highlight)
+
+  it('should render content with basic markup', () => {
+    expect(highlightDOMNode.className).to.contain('hljs')
+    expect(highlightDOMNode.querySelector('code').className).to.contain('javascript')
+    expect(highlightDOMNode.querySelector('.hljs-keyword').textContent).to.contain('var')
+    expect(highlightDOMNode.querySelector('.hljs-variable').textContent).to.contain('foo')
+    expect(highlightDOMNode.querySelector('.hljs-string').textContent).to.contain('"Foo"')
+    expect(highlightDOMNode.querySelector('.hljs-comment').textContent).to.contain('// comment')
+  })
 })
