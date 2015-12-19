70f993f059e03ff3591900f2a0fc3d3014a5a90e
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-12-19 09:33:20 -0800

    Improved example and READMEg

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ See the demo [here](http://bvaughn.github.io/react-highlight.js/).
 
 # Installation
 
-Install this component using NPM like so:
+Install react-highlight.js using NPM:
 ```bash
 npm install react-highlight.js --save
+```
+
+Choose a highlight.js theme and make sure it's included in your index file.
+```html
+<link rel='stylesheet' href='https://highlightjs.org/static/demo/styles/railscasts.css'/>
+```
+
+And then use react-highlight.js to display your text like so:
+```html
+<Highlight language={language}>
+  {content}
+</Highlight>
 ```
 
 # License
--- website/Application.js
+++ website/Application.js
@@ -18,9 +18,20 @@ export default class Application extends React.Component {
         </p>
         <h2>Demo</h2>
         <HighlightDemo />
-        <h2>Installation</h2>
+        <h2>Usage</h2>
+        <p>Install react-highlight.js using NPM</p>
         <Highlight language='bash'>
           npm install react-highlight.js --save
+        </Highlight>
+        <p>Choose a highlight.js theme and make sure it's included in your index file.</p>
+        <Highlight language='html'>
+          {`<link rel='stylesheet' href='https://highlightjs.org/static/demo/styles/railscasts.css'/>`}
+        </Highlight>
+        <p>And then use react-highlight.js to display your text like so:</p>
+        <Highlight language='html'>
+{`<Highlight language={language}>
+  {content}
+</Highlight>`}
         </Highlight>
         <h2>License</h2>
         <p>react-highlight.js is available under the MIT License.</p>
