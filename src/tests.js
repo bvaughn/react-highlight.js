0343a07dba9e35b4aa2245f6ace35db249db8fe4
Glenn Reyes <office@glennreyes.com>
2016-05-07 14:21:24 +0200

    Updated node packages & pass tests

--- src/tests.js
+++ src/tests.js
@@ -1,7 +1,7 @@
 // Reference: https://babeljs.io/docs/usage/polyfill/
 // Reference: https://github.com/zloirock/core-js
 // Polyfill a full ES6 environment
-import 'babel/polyfill'
+import 'babel-polyfill'
 
 // Reference: https://github.com/webpack/karma-webpack#alternative-usage
 const tests = require.context('.', true, /\.test\.js$/)
